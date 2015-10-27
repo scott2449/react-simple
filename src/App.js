ebb37ec25b8dae37cdf47697d94735c4080809fd
Scott Rahner <Scott Rahner>
2015-10-27 00:46:28 -0400

    playing with lint

--- src/App.js
+++ src/App.js
@@ -8,38 +8,41 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://www.reddit.com/r/news/.json").then((res) => {
-      res.json().then((feed) => this.setState({posts: feed.data.children}))
-    })
+    fetch('https://www.reddit.com/r/news/.json').then((res) => {
+      res.json().then((feed) => this.setState({posts: feed.data.children}));
+    });
   }
 
   render() {
-    return (
-      <Feed posts={this.state.posts} />
-    );
+    return <Feed posts={this.state.posts} />;
   }
 
 }
 
 class Feed extends Component {
+  propTypes = {
+    posts: React.PropTypes.any.isRequired
+  }
+
   render() {
-    return (
-      <div>
+    return <div>
         {this.props.posts.map((post, key)=>{
-          return <FeedItem key={key} {...post.data} />
+          return <FeedItem key={key} {...post.data} />;
         })}
-      </div>
-    );
+      </div>;
   }
 }
 
 class FeedItem extends Component {
+  propTypes = {
+    title: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired
+  }
+
   render() {
-    return (
-      <div>
+    return <div>
         <div>{this.props.title}</div>
         <div>{this.props.url}</div>
-      </div>
-    );
+      </div>;
   }
 }
